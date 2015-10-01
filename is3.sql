b2cd9a8da0a4dcbae4dc19e8db10dea5bc4609ee
Sebatl <Seba_tl@hotmail.com>
2015-10-01 18:12:04 -0300

    Actualización de la BD
    Nuevas tablas agregadas

--- is3.sql
+++ is3.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-10-2015 a las 01:03:48
+-- Tiempo de generación: 01-10-2015 a las 23:10:13
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -23,6 +23,97 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pharmacy`
+--
+
+CREATE TABLE IF NOT EXISTS `pharmacy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `pharmacy`
+--
+
+INSERT INTO `pharmacy` (`id`, `name`) VALUES
+(1, 'Test1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `phsucursal`
+--
+
+CREATE TABLE IF NOT EXISTS `phsucursal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pharmacy_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `phsucursal`
+--
+
+INSERT INTO `phsucursal` (`id`, `pharmacy_id`, `name`, `address`) VALUES
+(1, 1, 'Sucursal 1', '8 de Octubre 2738');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pharmacy_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` float NOT NULL,
+  `desc` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product_sucursal`
+--
+
+CREATE TABLE IF NOT EXISTS `product_sucursal` (
+  `product_id` int(11) NOT NULL,
+  `sucursal_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale`
+--
+
+CREATE TABLE IF NOT EXISTS `sale` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` int(11) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale_product`
+--
+
+CREATE TABLE IF NOT EXISTS `sale_product` (
+  `sale_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
@@ -33,8 +124,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `VALIDATIONCODE` varchar(255) DEFAULT NULL,
   `ACTIVATED` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`ID`, `EMAIL`, `PASSWORD`, `VALIDATIONCODE`, `ACTIVATED`) VALUES
+(1, 'qwe', 'a51c008b213c7a0462e49895df164b50', '123456789', 1),
+(25, 'Seba_tl@hotmail.com', 'a5c7cbc5c4e60094e7ff66762aff0bc5', 'qireerocvcnivgn635n0nemb6i783fsjpa9oeo15qkff', 1),
+(26, 'Sebastiantorreslemos@gmail.com', '131c96754fb6a66f20f5e36b3332cd8e', 'viu0ijtcdlt6dj383kfvap9xczer7sunrhsjgkbbi4xi', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
